ecf07625a5b6f309cdd008d9a055904d0bbb360e
siddarth-tech <59189853+siddarth-tech@users.noreply.github.com>
2019-12-28 01:40:47 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,9 +1,9 @@
-class Test{
+class Test {
 	public static void main(String[] args){
 		               int a,b,c;
 					a = 20;
 					b = 40; 
-					c = 25;
-					System.out.println("\n the sum of the three values");
+		                        int d = 0;
+					System.out.println(d=a+b+c);
 				}}	
 
